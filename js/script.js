50f2c26be60486aa10501b3a7d2f1db4c7cf2e26
EliseoVM <elyzeo.vm@gmail.com>
2019-10-14 16:49:57 -0500

    Cambiando el CSS de un elemento con JQuery

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 $(function() {
     'use strict';
 
-    // Cambiando imagen al hacer click
-    $('main article:first img').on('click', function() {
-        $(this).attr('src', 'img/imagen_2.jpg');
+    $('.navegacion').show();
+
+    // Cambiamos el tamaño del logo
+    $('.logo img').css({ 'width': '200px' });
+
+    // Cambiamos el color de los titulos de los articulos
+    $('main article h2').css({ 'color': '#db008d' });
+
+    // Cambiamos las caracteristicas del aside
+    $('aside').css({
+        'background-color': '#e1e1e1',
+        'text-transform': 'uppercase',
+        'padding': '20px'
     });
 
+    // Cambiamos el color de la barra de navegacion cuando el mouse pasa
+    $('.navegacion ul li a').on('mouseenter', cambiarColor);
+
+    function cambiarColor() {
+        $('.navegacion').css('brackground-color', 'red');
+    }
 
 
 });
