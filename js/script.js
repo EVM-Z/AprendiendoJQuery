95051a588ac9a503ca6d1ddcebf75837fbb14c08
EliseoVM <elyzeo.vm@gmail.com>
2019-10-14 12:34:58 -0500

    Seleccionando y cambiando atributos con JQuery

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 $(function() {
     'use strict';
 
-    $('main').on({
-        click: function() {
-            $(this).addClass('fondorojo activo');
-        },
-        mouseenter: function() {
-            // Aplicamos la clase fondorojo que esta en CSS
-            $(this).addClass('fondorojo');
-        },
-        mouseleave: function() {
-            $(this).addClass('activo');
-        }
+    // Cambiando imagen al hacer click
+    $('main article:first img').on('click', function() {
+        $(this).attr('src', 'img/imagen_2.jpg');
     });
+
 
 
 });
