ffc993006cd2355f8a3ceda95dbdcce6e9b8fcdb
EliseoVM <elyzeo.vm@gmail.com>
2019-10-15 14:34:42 -0500

    Ajax con JQuery

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 $(function() {
     'use strict';
 
-    // Escondemos el primer articulo al hacer click en el logo
-    $('.logo img').click(function() {
-        $('main article:first').slideUp(1000);
-    });
+    $('div.logo img').on('click', cargarAjax);
 
-    // Escondemos el primer articulo y lo volvemos a aparecer con el aside que esta a la derecha
-    $('aside').click(function() {
-        $('main article:first').slideDown(1000);
-    });
+    function cargarAjax() {
+        $.ajax('promociones.txt', {
+            success: agregarContenido,
+            type: 'GET',
+            datatype: 'text'
+        });
+    }
+
+    // Mostramos el contenido del txt al hacer click en el logo
+    function agregarContenido(data, status, jqxhr) {
+        // Donde mandamos los datos
+        $('aside').text(data);
+        // Mostramos en consola
+        console.log(status);
+    }
 
 });
