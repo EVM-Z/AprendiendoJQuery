f567db571b01024c482c9ce3851db9636de4caee
EliseoVM <elyzeo.vm@gmail.com>
2019-10-15 14:14:31 -0500

    Eventos en JQuery

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 $(function() {
     'use strict';
 
-    $('.navegacion').show();
-
-    // Cambiamos el tamaño del logo
-    $('.logo img').css({ 'width': '200px' });
-
-    // Cambiamos el color de los titulos de los articulos
-    $('main article h2').css({ 'color': '#db008d' });
-
-    // Cambiamos las caracteristicas del aside
-    $('aside').css({
-        'background-color': '#e1e1e1',
-        'text-transform': 'uppercase',
-        'padding': '20px'
+    $('.logo img').on('click', function() {
+        // Encojemos el logo por despues de un click y por medio de una animacion
+        $(this).animate({ 'width': '200px' }, 1000);
     });
 
-    // Cambiamos el color de la barra de navegacion cuando el mouse pasa
-    $('.navegacion ul li a').on('mouseenter', cambiarColor);
+    $('main article img').on('mouseenter', aumentarImagen);
+    $('main article img').on('mouseleave', disminuirImagen);
 
-    function cambiarColor() {
-        $('.navegacion').css('brackground-color', 'red');
+    function aumentarImagen() {
+        $(this).animate({ 'width:': '100%' });
+    }
+
+    function disminuirImagen() {
+        $(this).animate({ 'width': '350px' });
     }
 
 
