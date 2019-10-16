37b36b910336407c8ccc4caff649c008541ab54a
EliseoVM <elyzeo.vm@gmail.com>
2019-10-16 15:56:08 -0500

    Escribiendo una funcion para reaccionar en base...

--- js/script.js
+++ js/script.js
@@ -7,6 +7,24 @@ $(function() {
     // Remarcamos la primera seccion en la que estamos
     $('.servicios nav a:first').addClass('activo');
 
+    $('.servicios nav a').on('click', mostrarTaps);
 
+    function mostrarTaps() {
+        // Remueve el ultimo click en la seleccion
+        $('.servicios nav a').removeClass('activo');
+
+        // Conserva el ultimo elemento resaltado
+        $(this).addClass('activo');
+
+        // Tomamos el valor del ultimo click con this
+        var enlace = $(this).attr('href');
+        // Ocultamos el div que NO se le haiga hecho click
+        $('.nuestros-servicios div').fadeOut();
+        // Mostramos la informacion
+        $(enlace).fadeIn();
+
+        // Evitamos el movimiento de pagina en cada click
+        return false;
+    }
 
 });
